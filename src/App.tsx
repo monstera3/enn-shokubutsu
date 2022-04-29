e488011c6a9e9e602f5b089bf043088e74a8d197
natsumi <aka.723n@gmail.com>
2022-04-29 16:06:43 +0900

    refactor: LogoImgコンポーネントを抽出

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import LogoImg from './components/LogoImg';
 
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
+      <LogoImg/>
     </div>
   );
 }
