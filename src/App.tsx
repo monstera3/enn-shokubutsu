f42c202e1bb0337f289cb1b85a7be16a4b3c94ff
natsumi <aka.723n@gmail.com>
2022-04-30 20:59:24 +0900

    feat: createTheme設定

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,60 @@
 import React from 'react';
 import './App.css';
 import LogoImg from './components/LogoImg';
-import { AppBar, Box, IconButton, Toolbar, Typography } from '@mui/material';
+import {
+  AppBar,
+  Box,
+  createTheme,
+  IconButton,
+  ThemeProvider,
+  Toolbar,
+  Typography
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+
+const sampleTheme = createTheme({
+  palette: {
+    primary: {
+      main: "#AFCECC",
+      light: "#fbf8f8",
+      dark: "#1565c0"
+    }
+  }
+});
 
 function App() {
   return (
-    <div className="App">
-      <Box sx={{ flexGrow: 1 }}>
-        <AppBar position="fixed">
-          <Toolbar>
-            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-              Enn
-            </Typography>
-            <IconButton
-              size="large"
-              edge="end"
-              color="inherit"
-              aria-label="menu"
-            >
-              <MenuIcon />
-            </IconButton>
+    <ThemeProvider theme={sampleTheme}>
+      <div className="App">
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="fixed">
+            <Toolbar>
+              <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                Enn
+              </Typography>
+              <IconButton
+                size="large"
+                edge="end"
+                color="inherit"
+                aria-label="menu"
+              >
+                <MenuIcon />
+              </IconButton>
 
 
-          </Toolbar>
-        </AppBar>
-      </Box>
-      <LogoImg/>
-      <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-        Enn
-      </Typography>
-      <LogoImg/>
-      <LogoImg/>
-      <LogoImg/>
-      <LogoImg/>
-    </div>
+            </Toolbar>
+          </AppBar>
+        </Box>
+        <LogoImg/>
+        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          Enn
+        </Typography>
+        <LogoImg/>
+        <LogoImg/>
+        <LogoImg/>
+        <LogoImg/>
+      </div>
+    </ThemeProvider>
   );
 }
 
