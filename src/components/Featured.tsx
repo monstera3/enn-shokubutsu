8d445d3cc4da90c995a14e1a04dea15e219cb52e
natsumi <aka.723n@gmail.com>
2022-05-01 22:01:35 +0900

    feat: sample1 image追加、View All調整

--- src/components/Featured.tsx
+++ src/components/Featured.tsx
@@ -1,6 +1,6 @@
 import React, { FC } from 'react';
 import { Box, Typography, useTheme } from '@mui/material';
-
+import sampleImg from '../assets/images/sample1.png'
 export const Featured:FC =()=> {
 
   const theme = useTheme();
@@ -11,12 +11,18 @@ export const Featured:FC =()=> {
         <Typography  variant="h5" sx={{textAlign: 'center',fontWeight:'light'}}>
           Featured
         </Typography>
-        <Box sx={{  width:'35%',textAlign: 'right',py:5}}>
-          <Typography sx={{ fontSize: '0.1rem',fontWeight:'light'}}>INDOOR PLANTS</Typography>
+        <Box sx={{  width:'35%',textAlign: 'right',pt:5}}>
+          <Typography sx={{ fontSize: '0.6rem',fontWeight:'light'}}>INDOOR PLANTS</Typography>
           <Box sx={{ borderTop:1, width:'100%'}}/>
           <Typography variant="h5">Monstera</Typography>
         </Box>
-        <img src="" alt=""/>
+
+        <Box sx={{ width:'65%',textAlign: 'center',py:5,mx:'auto'}}>
+          <img  src={sampleImg} alt=""/>
+
+          <Typography sx={{ textAlign: 'center',fontWeight:'light',mt:3,ml:3}}>View All</Typography>
+          <Box  sx={{ borderTop:1, width:'60%',ml:13}}/>
+        </Box>
       </Box>
     </div>
   );
